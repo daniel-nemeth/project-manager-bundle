4d124e89039083a7c72decdb8aa48ad4117131c8
Daniel Nemeth <office@daniel-nemeth.com>
2018-11-03 18:39:22 +0100

    Translated to 'en'

--- src/Resources/contao/languages/en/default.php
+++ src/Resources/contao/languages/en/default.php
@@ -15,18 +15,18 @@
  * permanent text corrections which are update-safe.
  */
 
-$GLOBALS['TL_LANG']['tl_iao']['types']['offer']	= 'Angebot';
-$GLOBALS['TL_LANG']['tl_iao']['types']['invoice']	= 'Rechnung';
-$GLOBALS['TL_LANG']['tl_iao']['types']['credit']	= 'Gutschrift';
+$GLOBALS['TL_LANG']['tl_iao']['types']['offer']	= 'Offer';
+$GLOBALS['TL_LANG']['tl_iao']['types']['invoice']	= 'Invoice';
+$GLOBALS['TL_LANG']['tl_iao']['types']['credit']	= 'Credit voucher';
 
-$GLOBALS['TL_LANG']['tl_iao']['active']	= array('aktiv', '');
+$GLOBALS['TL_LANG']['tl_iao']['active']	= array('active', '');
 $GLOBALS['TL_LANG']['tl_iao']['status']['title']	= array('Status', '');
 $GLOBALS['TL_LANG']['tl_iao']['counter']	= array('Counter','');
 
-$GLOBALS['TL_LANG']['tl_iao']['credit_status'][0]	= array('nicht angenommen', '');
-$GLOBALS['TL_LANG']['tl_iao']['credit_status'][1]	= array('angenommen', '');
+$GLOBALS['TL_LANG']['tl_iao']['credit_status'][0]	= array('not accepted', '');
+$GLOBALS['TL_LANG']['tl_iao']['credit_status'][1]	= array('accepted', '');
 
-$GLOBALS['TL_LANG']['tl_iao']['gender']['male']	=  'Herr';
-$GLOBALS['TL_LANG']['tl_iao']['gender']['female']	=  'Frau';
+$GLOBALS['TL_LANG']['tl_iao']['gender']['male']	=  'Mr';
+$GLOBALS['TL_LANG']['tl_iao']['gender']['female']	=  'Ms';
 
-$GLOBALS['TL_LANG']['ERR']['PermissionDenied'] = 'keine Berechtigung für %s';
+$GLOBALS['TL_LANG']['ERR']['PermissionDenied'] = 'no permission for %s';
