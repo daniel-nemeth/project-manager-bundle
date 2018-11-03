8874bab618e6321b715c3e24c709b6b274fc6f49
Daniel Nemeth <office@daniel-nemeth.com>
2018-11-03 21:53:20 +0100

    Translated to 'en'

--- src/Resources/contao/languages/en/tl_iao_projects.php
+++ src/Resources/contao/languages/en/tl_iao_projects.php
@@ -15,51 +15,51 @@
  * permanent text corrections which are update-safe.
  */
 
-$GLOBALS['TL_LANG']['tl_iao_projects']['setting_id']	=	array('Konfiguration','');
-$GLOBALS['TL_LANG']['tl_iao_projects']['title']	=	array('Bezeichnung','Bezeichnung des Elementes');
-$GLOBALS['TL_LANG']['tl_iao_projects']['member']	=	array('Kunde','Adresse aus gespeicherten Kunden in nachstehendes Feld befüllen');
-$GLOBALS['TL_LANG']['tl_iao_projects']['reference_title']	=	array('Titel für die Referenzen','');
-$GLOBALS['TL_LANG']['tl_iao_projects']['reference_customer']	=	array('Kunden-Beschreibung','');
-$GLOBALS['TL_LANG']['tl_iao_projects']['reference_todo']	=	array('Aufgaben','');
-$GLOBALS['TL_LANG']['tl_iao_projects']['reference_desription']	=	array('Projekt-Beschreibung','');
-$GLOBALS['TL_LANG']['tl_iao_projects']['in_reference']	=	array('in Referenzen aufnehmen.','');
-$GLOBALS['TL_LANG']['tl_iao_projects']['finished']	=	array('fertig gestellt','');
-$GLOBALS['TL_LANG']['tl_iao_projects']['singleSRC']	=	array('Projekt-Bild','wird für alle Projekt-Teaser als Hauptbild verwendet');
-$GLOBALS['TL_LANG']['tl_iao_projects']['multiSRC']	=	array('Projekt-Bild-Galerie','');
+$GLOBALS['TL_LANG']['tl_iao_projects']['setting_id']	=	array('Configuration','');
+$GLOBALS['TL_LANG']['tl_iao_projects']['title']	=	array('Title','Title of the element');
+$GLOBALS['TL_LANG']['tl_iao_projects']['member']	=	array('Customer','Add address of saved customer into the following field');
+$GLOBALS['TL_LANG']['tl_iao_projects']['reference_title']	=	array('Title for references','');
+$GLOBALS['TL_LANG']['tl_iao_projects']['reference_customer']	=	array('Customer description','');
+$GLOBALS['TL_LANG']['tl_iao_projects']['reference_todo']	=	array('Tasks','');
+$GLOBALS['TL_LANG']['tl_iao_projects']['reference_desription']	=	array('Project description','');
+$GLOBALS['TL_LANG']['tl_iao_projects']['in_reference']	=	array('add to reference.','');
+$GLOBALS['TL_LANG']['tl_iao_projects']['finished']	=	array('finished','');
+$GLOBALS['TL_LANG']['tl_iao_projects']['singleSRC']	=	array('Project image','Will be used for all project teasers as main image');
+$GLOBALS['TL_LANG']['tl_iao_projects']['multiSRC']	=	array('Project gallery','');
 $GLOBALS['TL_LANG']['tl_iao_projects']['url']	=	array('www','Website-URL');
-$GLOBALS['TL_LANG']['tl_iao_projects']['notice']	=	array('Notiz','Diese Notizen werden ausschließlich im Backend verwendet.');
+$GLOBALS['TL_LANG']['tl_iao_projects']['notice']	=	array('Notice','The notice will be used in backend only.');
 
 /**
  * Buttons
  */
-$GLOBALS['TL_LANG']['tl_iao_projects']['offer']	=	array('Projekt-Angebote','alle Angebote von diesem Projekt');
-$GLOBALS['TL_LANG']['tl_iao_projects']['invoice']	=	array('Projekt-Rechnungen','alle Rechnungen von diesem Projekt');
-$GLOBALS['TL_LANG']['tl_iao_projects']['credit']	=	array('Projekt-Gutschriften','alle Gutschriften vone diesem Projekt');
-$GLOBALS['TL_LANG']['tl_iao_projects']['reminder']	=	array('Projekt-Erinnerungen','alle Erinnerungen von diesem Projekt');
-$GLOBALS['TL_LANG']['tl_iao_projects']['agreements']	=	array('Projekt-Verträge','alle Verträge von diesem Projekt');
-$GLOBALS['TL_LANG']['tl_iao_projects']['new']	=	array('Neues Projekt','Ein neues Projekt anlegen');
-$GLOBALS['TL_LANG']['tl_iao_projects']['edit']	=	array('Projekt bearbeiten','Projekt ID %s bearbeiten');
-$GLOBALS['TL_LANG']['tl_iao_projects']['copy']	=	array('Projekt duplizieren','Projekt ID %s duplizieren');
-$GLOBALS['TL_LANG']['tl_iao_projects']['delete']	=	array('Projekt löschen','Projekt ID %s löschen');
-$GLOBALS['TL_LANG']['tl_iao_projects']['deleteConfirm']	=	'Soll das Projekt mit der ID %s wirklich gelöscht werden?!';
-$GLOBALS['TL_LANG']['tl_iao_projects']['show']	=	array('Details anzeigen','Details vom Projekt ID %s anzeigen');
+$GLOBALS['TL_LANG']['tl_iao_projects']['offer']	=	array('Project offers','All offers for this project');
+$GLOBALS['TL_LANG']['tl_iao_projects']['invoice']	=	array('Project invoices','All invoices for this project');
+$GLOBALS['TL_LANG']['tl_iao_projects']['credit']	=	array('Project credit vouchers','All credit vouchers for this project');
+$GLOBALS['TL_LANG']['tl_iao_projects']['reminder']	=	array('Project reminders','All reminders for this project');
+$GLOBALS['TL_LANG']['tl_iao_projects']['agreements']	=	array('Project agreements','All agreements for this project');
+$GLOBALS['TL_LANG']['tl_iao_projects']['new']	=	array('Add new project','Add a new project');
+$GLOBALS['TL_LANG']['tl_iao_projects']['edit']	=	array('Edit project','Edit project ID %s');
+$GLOBALS['TL_LANG']['tl_iao_projects']['copy']	=	array('Duplicate project','Duplicate project ID %s');
+$GLOBALS['TL_LANG']['tl_iao_projects']['delete']	=	array('Delete project','Delete project ID %s');
+$GLOBALS['TL_LANG']['tl_iao_projects']['deleteConfirm']	=	'Do you really want to delete the project ID %s?!';
+$GLOBALS['TL_LANG']['tl_iao_projects']['show']	=	array('Show details','Show details of project ID %s');
 
 /**
  * Legends
  */
-$GLOBALS['TL_LANG']['tl_iao_projects']['settings_legend']	=	'Konfiguration-Zuweisung';
-$GLOBALS['TL_LANG']['tl_iao_projects']['project_legend']	=	'Projekt-Einstellungen';
-$GLOBALS['TL_LANG']['tl_iao_projects']['reference_legend']	=	'Referenz-Zuweisung';
+$GLOBALS['TL_LANG']['tl_iao_projects']['settings_legend']	=	'Configuration assignment';
+$GLOBALS['TL_LANG']['tl_iao_projects']['project_legend']	=	'Project settings';
+$GLOBALS['TL_LANG']['tl_iao_projects']['reference_legend']	=	'Reference assignment';
 
 /**
 * Frontend-Templates
 */
-$GLOBALS['TL_LANG']['tl_iao_projects']['fe_table_head']['title'] = 'Titel/ Rechnungsnr.:';
-$GLOBALS['TL_LANG']['tl_iao_projects']['fe_table_head']['date'] = 'erstellt am:';
-$GLOBALS['TL_LANG']['tl_iao_projects']['fe_table_head']['price'] = 'Betrag:';
-$GLOBALS['TL_LANG']['tl_iao_projects']['fe_table_head']['remaining'] = 'offen:';
+$GLOBALS['TL_LANG']['tl_iao_projects']['fe_table_head']['title'] = 'Title / Invoice number:';
+$GLOBALS['TL_LANG']['tl_iao_projects']['fe_table_head']['date'] = 'created on:';
+$GLOBALS['TL_LANG']['tl_iao_projects']['fe_table_head']['price'] = 'Amount:';
+$GLOBALS['TL_LANG']['tl_iao_projects']['fe_table_head']['remaining'] = 'Open:';
 $GLOBALS['TL_LANG']['tl_iao_projects']['fe_table_head']['file'] = 'PDF:';
 
 
 // Meldungen
-$GLOBALS['TL_LANG']['tl_iao_projects']['no_entries_msg'] = 'Es sind keine Einträge für diesen Bereich vorhanden.';
+$GLOBALS['TL_LANG']['tl_iao_projects']['no_entries_msg'] = 'There are no entries for this type available.';
