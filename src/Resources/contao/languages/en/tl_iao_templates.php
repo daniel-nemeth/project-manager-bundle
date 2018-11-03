2bdf813f01fc9a443b319522e985e4f236572b21
Daniel Nemeth <office@daniel-nemeth.com>
2018-11-03 21:45:25 +0100

    Translated to 'en'

--- src/Resources/contao/languages/en/tl_iao_templates.php
+++ src/Resources/contao/languages/en/tl_iao_templates.php
@@ -15,19 +15,19 @@
  * permanent text corrections which are update-safe.
  */
 
-$GLOBALS['TL_LANG']['tl_iao_templates']['title'] = array('Bezeichnung','Bezeichnung der Vorlage');
-$GLOBALS['TL_LANG']['tl_iao_templates']['text'] = array('Vorlagen-Text','');
+$GLOBALS['TL_LANG']['tl_iao_templates']['title'] = array('Title','Title for the template');
+$GLOBALS['TL_LANG']['tl_iao_templates']['text'] = array('Template text','');
 $GLOBALS['TL_LANG']['tl_iao_templates']['position'] = array('Position','');
-$GLOBALS['TL_LANG']['tl_iao_templates']['new'] = array('Neue Vorlage','Eine neue Vorlage anlegen');
-$GLOBALS['TL_LANG']['tl_iao_templates']['edit'] = array('Vorlage bearbeiten','Vorlage ID %s bearbeiten');
-$GLOBALS['TL_LANG']['tl_iao_templates']['copy'] = array('Vorlage duplizieren','Vorlage ID %s duplizieren');
-$GLOBALS['TL_LANG']['tl_iao_templates']['delete'] = array('Vorlage löschen','Vorlage ID %s löschen');
+$GLOBALS['TL_LANG']['tl_iao_templates']['new'] = array('New template','Add a new template');
+$GLOBALS['TL_LANG']['tl_iao_templates']['edit'] = array('Edit template','Edit template ID %s');
+$GLOBALS['TL_LANG']['tl_iao_templates']['copy'] = array('Duplicate templaten','Duplicate template ID %s');
+$GLOBALS['TL_LANG']['tl_iao_templates']['delete'] = array('Delete template','Delete template ID %s');
 
 $GLOBALS['TL_LANG']['tl_iao_templates']['template_options'] = array(
-    'invoice_before_text'=>'Rechnung Text vor Positionen',
-    'invoice_after_text'=>'Rechnung Text nach Positionen',
-    'offer_before_text'=>'Angebot Text vor Positionen',
-    'offer_after_text'=>'Angebot Text nach Positionen',
-    'credit_before_text'=>'Gutschrift Text vor Positionen',
-    'credit_after_text'=>'Gutschrift Text nach Positionen',
+    'invoice_before_text'=>'Invoice text before items',
+    'invoice_after_text'=>'Invoice text after items',
+    'offer_before_text'=>'Offer text before items',
+    'offer_after_text'=>'Offer text after items',
+    'credit_before_text'=>'Credit voucher text before items',
+    'credit_after_text'=>'Credit voucher text after items',
     );
