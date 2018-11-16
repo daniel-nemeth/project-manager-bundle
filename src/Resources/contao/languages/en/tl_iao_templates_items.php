ba866c88fa397166a6a2c8b257a8a5f3badd4324
Daniel Nemeth <office@daniel-nemeth.com>
2018-11-16 19:06:48 +0100

    Translated to 'en'

--- src/Resources/contao/languages/en/tl_iao_templates_items.php
+++ src/Resources/contao/languages/en/tl_iao_templates_items.php
@@ -15,29 +15,29 @@
  * permanent text corrections which are update-safe.
  */
 
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['title'] = array('Bezeichnung','Bezeichnung der Vorlage');
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['text'] = array('Vorlagen-Text','');
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['title'] = array('Title','Title for this template');
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['text'] = array('Template-Text','');
 $GLOBALS['TL_LANG']['tl_iao_templates_items']['position'] = array('Position','');
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['headline'] = array('Bezeichnung','Posten-Bezeichnung');
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['headline_to_pdf'] = array('Bezeichnung in PDF aufnhemen','wenn die Bezeichnung in der PDF-Datei vor dem Text aufgenommen werden soll.');
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['text'] = array('Beschreibung','hier wird die Beschreibung zu dem Posten eingegeben.');
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['price'] = array('Preis','geben Sie hier den Preis an.');
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['vat_incl'] = array('Preis-Angabe mit oder ohne MwSt.','(brutto / netto)');
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['count'] = array('Anzahl','die Anzahl wird mit dem Preis multipliziert');
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['amountStr'] = array('Art der Anzahl','');
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['operator'] = array('Zahlungsart','soll dieser Posten hinzugefügt oder abgezogen werden?');
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['vat'] = array('MwSt.','Art der MwSt. zu diesem Posten.');
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['published'] = array('veröffentlicht');
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['new'] = array('Neue Posten-Vorlage','Eine neue Vorlage anlegen');
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['edit'] = array('Posten-Vorlage bearbeiten','Vorlage ID %s bearbeiten');
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['copy'] = array('Posten-Vorlage duplizieren','Vorlage ID %s duplizieren');
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['delete'] = array('posten-Vorlage löschen','Vorlage ID %s löschen');
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['headline'] = array('Headline','Item title');
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['headline_to_pdf'] = array('Add headline to PDF','Should the headline be added within the PDF file before the text?');
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['text'] = array('Description','Add a description for this item.');
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['price'] = array('Price','Please enter an price.');
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['vat_incl'] = array('Price with or without VAT','(gross / net)');
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['count'] = array('Quantity','The quantity will be multiplicated with the price');
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['amountStr'] = array('Type of quantity','');
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['operator'] = array('Type for payment','Should this item be added or substraced?');
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['vat'] = array('VAT','The type of VAT for this item.');
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['published'] = array('published');
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['new'] = array('New item template','Add a new item template');
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['edit'] = array('Edit item template','Edit item template ID %s');
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['copy'] = array('Duplicate item template','Duplicate item template ID %s');
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['delete'] = array('Delete item template','Delete item template ID %s');
 
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['title_legend'] = 'Grundeinstellungen';
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['item_legend'] = 'Posten-Daten';
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['publish_legend'] = 'Veröffentlichung';
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['title_legend'] = 'Default settings';
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['item_legend'] = 'Item data';
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['publish_legend'] = 'Publishing';
 
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['vat_incl_percents'] = array(1 => 'netto', 2 => 'brutto');
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['operators'] = array('+' => '+ (addieren)', '-' => '- (subtrahieren)');
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['type_options'] = array('item'=>'Eintrag','devider'=>'PDF-Trenner');
-$GLOBALS['TL_LANG']['tl_iao_templates_items']['position_options'] = array('offer'=>'Angebot', 'credit'=>'Gutschrift', 'invoice'=>'Rechnung');
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['vat_incl_percents'] = array(1 => 'net', 2 => 'gross');
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['operators'] = array('+' => '+ (total)', '-' => '- (substract)');
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['type_options'] = array('item'=>'Item','devider'=>'PDF Divider');
+$GLOBALS['TL_LANG']['tl_iao_templates_items']['position_options'] = array('offer'=>'Offer', 'credit'=>'Credit voucher', 'invoice'=>'Invoice');
